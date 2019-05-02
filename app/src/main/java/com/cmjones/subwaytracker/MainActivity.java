e385f74f39a6c7a38f943c85be14c5e954094433
Cody M. Jones <codymjones@protonmail.com>
2019-05-02 15:57:39 -0500

    Create new request queue

--- app/src/main/java/com/cmjones/subwaytracker/MainActivity.java
+++ app/src/main/java/com/cmjones/subwaytracker/MainActivity.java
@@ -2,6 +2,8 @@ package com.cmjones.subwaytracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -15,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestQueue = Volley.newRequestQueue(this);
+
         super.onCreate(savedInstanceState);
+
+        // Load the main layout
         setContentView(R.layout.activity_main);
     }
 }
