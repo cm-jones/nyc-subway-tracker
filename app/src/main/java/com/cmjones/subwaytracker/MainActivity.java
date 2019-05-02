88fe950b2351053c97361c594d498aaa19b5d9cd
Cody M. Jones <codymjones@protonmail.com>
2019-05-02 15:53:52 -0500

    Add Volley library to project

--- app/src/main/java/com/cmjones/subwaytracker/MainActivity.java
+++ app/src/main/java/com/cmjones/subwaytracker/MainActivity.java
@@ -3,9 +3,15 @@ package com.cmjones.subwaytracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "nyc-subway-tracker:Main";
+
+    /** Request queue for network requests. */
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
